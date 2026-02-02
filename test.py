e1a6b9b5c6a61c24bfdc3799acee40b0da8caf28
Nouha_Karaouli <98420058+Nouha44@users.noreply.github.com>
2026-02-02 15:01:38 +0100

    Update CSV_PATH to point to mult_sin_d4_full.csv

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ from model.S_Mamba import Model
 # =====================================================
 # CONFIG
 # =====================================================
-CSV_PATH = "/home/nkaraoul/timesfm_backup/mult_sin_d1_full.csv"
+CSV_PATH = "/home/nkaraoul/timesfm_backup/mult_sin_d4_full.csv"
 SEQ_LEN = 128
 PRED_LEN = 128
 LABEL_LEN = SEQ_LEN // 2
