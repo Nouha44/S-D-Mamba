f7fe9461656d1f0c4a3bcf53f131c1d08a541b31
Nouha_Karaouli <98420058+Nouha44@users.noreply.github.com>
2026-02-02 14:53:57 +0100

    Update CSV_PATH to new file location

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ from model.S_Mamba import Model
 # =====================================================
 # CONFIG
 # =====================================================
-CSV_PATH = "./dataset/weather/weather.csv"
+CSV_PATH = "/home/nkaraoul/timesfm_backup/mult_sin_d1_full.csv"
 SEQ_LEN = 128
 PRED_LEN = 128
 LABEL_LEN = SEQ_LEN // 2
