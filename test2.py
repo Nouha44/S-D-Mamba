49e927ca8969a92aa5c952491f88a01ff95d7fd7
Nouha_Karaouli <98420058+Nouha44@users.noreply.github.com>
2026-02-02 17:11:17 +0100

    Fix import statement for Model in test2.py

--- test2.py
+++ test2.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
 from sklearn.preprocessing import StandardScaler
 
-from model.MSM-SSM import Model
+from model.MSM import Model
 
 # =====================================================
 # CONFIG
