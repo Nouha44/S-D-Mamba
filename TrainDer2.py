d783b4ee48f2f18b373e41cbbeeae8a3a3dc5f0d
Nouha_Karaouli <98420058+Nouha44@users.noreply.github.com>
2026-02-03 16:16:54 +0100

    Update TrainDer2.py

--- TrainDer2.py
+++ TrainDer2.py
@@ -150,7 +150,7 @@ def main():
         optimizer=optimizer,
         criterion=criterion,
         device=DEVICE,
-        replay_buffer_size=1500,
+        replay_buffer_size=0,
         alpha=1.0,
     )
 
