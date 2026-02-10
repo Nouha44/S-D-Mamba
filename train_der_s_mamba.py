0219ede3dde2c2b6b4f746ca68e51fabf557836e
Nouha_Karaouli <98420058+Nouha44@users.noreply.github.com>
2026-02-10 11:25:16 +0100

    Set replay buffer size to 200

--- train_der_s_mamba.py
+++ train_der_s_mamba.py
@@ -124,7 +124,7 @@ def main():
         optimizer=optimizer,
         criterion=criterion,
         device=DEVICE,
-        replay_buffer_size=0,
+        replay_buffer_size=200,
         alpha=0.0,
         beta=0,  # DER++ mixing parameter
         replay_mode="both"  # "labels", "logits" ou "both"
